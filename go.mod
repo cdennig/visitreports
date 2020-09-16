6b2efe383f962e3598821292e2f4dcb0e5cfded4
Christian Dennig / MS <dechrist@microsoft.com>
2020-09-16 17:23:22 +0200

    Temp version

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/cdennig/visitreports
 go 1.15
 
 require (
+	github.com/Azure/azure-service-bus-go v0.10.6
 	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/google/uuid v1.1.2
+	github.com/jinzhu/copier v0.0.0-20190924061706-b57f9002281a
 	github.com/joho/godotenv v1.3.0
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/kelseyhightower/envconfig v1.4.0
--- main.go
+++ main.go
@@ -2,26 +2,39 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
-	"github.com/google/uuid"
-
+	servicebus "github.com/Azure/azure-service-bus-go"
 	"github.com/fvbock/endless"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/jinzhu/copier"
 	"github.com/joho/godotenv"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/pkg/errors"
 	"github.com/vippsas/go-cosmosdb/cosmosapi"
 )
 
+type ListenerHandler struct {
+}
+
+func (l *ListenerHandler) Handle(c context.Context, m *servicebus.Message) error {
+	return nil
+}
+
 type config struct {
-	DbURL  string `required:"true"`
-	DbKey  string `required:"true"`
-	DbName string `required:"true"`
-	Env    string
+	DbURL                string `required:"true"`
+	DbKey                string `required:"true"`
+	DbName               string `required:"true"`
+	SbConnStrVisitReport string `required:"true"`
+	SbConnStrContact     string `required:"true"`
+	Env                  string
 }
 
 // ContactDoc - Base contact properties
@@ -31,6 +44,13 @@ type ContactDoc struct {
 	Lastname       string `json:"lastname"`
 	AvatarLocation string `json:"avatarLocation"`
 	Company        string `json:"company"`
+}
+
+// VisitReportEventDoc - struct for sending an event
+type VisitReportEventDoc struct {
+	EventType string `json:"eventType"`
+	Version   string `json:"version"`
+	VisitReportReadDoc
 }
 
 // VisitReportReadDoc - struct for reading a
@@ -57,6 +77,7 @@ type VisitReportCreateDoc struct {
 
 // VisitReportUpdateDoc - struct for updating a VR
 type VisitReportUpdateDoc struct {
+	ID          string     `json:"id" binding:"required" validate:"uuid"`
 	Type        string     `json:"type" binding:"-"`
 	Subject     string     `json:"subject" binding:"required" validate:"max=255"`
 	Description string     `json:"description" validate:"max=500"`
@@ -77,6 +98,7 @@ type VisitReportListDoc struct {
 var currentDb *cosmosapi.Database
 var currentClient *cosmosapi.Client
 var currentCfg *config
+var currentTopic *servicebus.Topic
 
 func fromEnv() config {
 	cfg := config{}
@@ -86,6 +108,45 @@ func fromEnv() config {
 	}
 
 	return cfg
+}
+
+func setupTopicSender() (*servicebus.Topic, error) {
+	ns, err := servicebus.NewNamespace(servicebus.NamespaceWithConnectionString(currentCfg.SbConnStrVisitReport))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	topic, err := ns.NewTopic("scmvrtopic")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return topic, nil
+}
+
+func setupSubscription() error {
+	ns, err := servicebus.NewNamespace(servicebus.NamespaceWithConnectionString(currentCfg.SbConnStrContact))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	topic, err := ns.NewTopic("scmtopic")
+	sub, err := topic.NewSubscription("scmcontactvisitreport")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	receiver, err := sub.NewReceiver(context.Background())
+	lHandle := receiver.Listen(context.Background(), servicebus.HandlerFunc(func(c context.Context, m *servicebus.Message) error {
+		return nil
+	}))
+
+	if lHandle == nil {
+		fmt.Println("Not init.")
+	}
+	return nil
 }
 
 func main() {
@@ -106,12 +167,31 @@ func main() {
 
 	// Get a database
 	db, err := currentClient.GetDatabase(context.Background(), currentCfg.DbName, nil)
+	if err != nil {
+		err = errors.WithStack(err)
+		fmt.Println(err)
+	}
+
 	currentDb = db
+
+	currentTopic, err = setupTopicSender()
+	setupSubscription()
 
 	if err != nil {
 		err = errors.WithStack(err)
 		fmt.Println(err)
 	}
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "DELETE", "PUT", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "accept", "origin", "Cache-Control", "X-Requested-With"},
+		ExposeHeaders:    []string{"Content-Length", "Location"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		MaxAge: 10 * time.Minute,
+	}))
 
 	r.GET("/", func(c *gin.Context) {
 		if currentClient != nil {
@@ -122,7 +202,9 @@ func main() {
 	})
 	r.GET("/reports", list)
 	r.GET("/reports/:reportid", read)
-	r.POST("/reports", update)
+	r.PUT("/reports/:reportid", update)
+	r.DELETE("/reports/:reportid", delete)
+	r.POST("/reports", create)
 	endless.ListenAndServe(":3000", r)
 }
 
@@ -153,7 +235,7 @@ func list(c *gin.Context) {
 		err = errors.WithStack(err)
 		fmt.Println(err)
 	}
-	c.JSON(200, docs)
+	c.JSON(http.StatusOK, docs)
 }
 
 func read(c *gin.Context) {
@@ -168,10 +250,24 @@ func read(c *gin.Context) {
 		err = errors.WithStack(err)
 		fmt.Println(err)
 	}
-	c.JSON(200, doc)
+	c.JSON(http.StatusOK, doc)
 }
 
-func update(c *gin.Context) {
+func delete(c *gin.Context) {
+	reportid := c.Param("reportid")
+	ro := cosmosapi.DeleteDocumentOptions{
+		PartitionKeyValue: "visitreport",
+	}
+
+	_, err := currentClient.DeleteDocument(context.Background(), currentCfg.DbName, "visitreports", reportid, ro)
+	if err != nil {
+		err = errors.WithStack(err)
+		fmt.Println(err)
+	}
+	c.Status(http.StatusOK)
+}
+
+func create(c *gin.Context) {
 
 	// Create visit report
 	var vr VisitReportCreateDoc
@@ -205,5 +301,87 @@ func update(c *gin.Context) {
 		errRead = errors.WithStack(errRead)
 		fmt.Println(errRead)
 	}
-	c.JSON(201, doc)
+
+	// send event
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	eventDoc := VisitReportEventDoc{}
+	copier.Copy(&eventDoc, &doc)
+	eventDoc.EventType = "VisitReportCreatedEvent"
+	eventDoc.Version = "1"
+	m, err := json.Marshal(eventDoc)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
+
+	sbMessage := servicebus.Message{
+		ContentType: "application/json",
+		Data:        m,
+	}
+	err = currentTopic.Send(ctx, &sbMessage)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, doc)
+}
+
+func update(c *gin.Context) {
+	reportid := c.Param("reportid")
+	// Create visit report
+	var vr VisitReportUpdateDoc
+	if bindError := c.ShouldBindJSON(&vr); bindError != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": bindError.Error()})
+		return
+	}
+
+	vr.Type = "visitreport"
+	ops := cosmosapi.ReplaceDocumentOptions{}
+	ops.PartitionKeyValue = "visitreport"
+
+	_, _, err := currentClient.ReplaceDocument(context.Background(), currentCfg.DbName, "visitreports", reportid, vr, ops)
+
+	if err != nil {
+		err = errors.WithStack(err)
+		fmt.Println(err)
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	ro := cosmosapi.GetDocumentOptions{
+		PartitionKeyValue: "visitreport",
+	}
+
+	var doc VisitReportReadDoc
+	_, errRead := currentClient.GetDocument(context.Background(), currentCfg.DbName, "visitreports", reportid, ro, &doc)
+	if errRead != nil {
+		errRead = errors.WithStack(errRead)
+		fmt.Println(errRead)
+	}
+
+	// send event
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	eventDoc := VisitReportEventDoc{}
+	copier.Copy(&eventDoc, &doc)
+	eventDoc.EventType = "VisitReportUpdatedEvent"
+	eventDoc.Version = "1"
+	m, err := json.Marshal(eventDoc)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
+
+	sbMessage := servicebus.Message{
+		ContentType: "application/json",
+		Data:        m,
+	}
+	err = currentTopic.Send(ctx, &sbMessage)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
+	c.JSON(http.StatusOK, doc)
 }
