c1566c798a35c4957db9c95cfd03d9c85e222351
Christian Dennig / MS <dechrist@microsoft.com>
2020-09-16 12:07:05 +0200

    Add update method Add env handling

--- main.go
+++ main.go
@@ -3,41 +3,75 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/google/uuid"
 
 	"github.com/fvbock/endless"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/pkg/errors"
 	"github.com/vippsas/go-cosmosdb/cosmosapi"
 )
 
 type config struct {
-	DbUrl  string
-	DbKey  string
-	DbName string
+	DbURL  string `required:"true"`
+	DbKey  string `required:"true"`
+	DbName string `required:"true"`
+	Env    string
 }
 
+// ContactDoc - Base contact properties
 type ContactDoc struct {
-	Id             string `json:"id"`
+	ID             string `json:"id" binding:"required"`
 	Firstname      string `json:"firstname"`
 	Lastname       string `json:"lastname"`
 	AvatarLocation string `json:"avatarLocation"`
 	Company        string `json:"company"`
 }
 
-type VisitReportDoc struct {
-	Id      string     `json:"id"`
-	Type    string     `json:"type"`
-	Subject string     `json:"subject"`
-	Contact ContactDoc `json:"contact"`
+// VisitReportReadDoc - struct for reading a
+type VisitReportReadDoc struct {
+	ID                        string     `json:"id"`
+	Subject                   string     `json:"subject"`
+	Description               string     `json:"description"`
+	VisitDate                 string     `json:"visitDate"`
+	Result                    string     `json:"result"`
+	VisitResultSentimentScore float64    `json:"visitResultSentimentScore"`
+	VisitResultKeyPhrases     []string   `json:"visitResultKeyPhrases"`
+	Contact                   ContactDoc `json:"contact"`
 }
 
+// VisitReportCreateDoc - struct for creating a VR
+type VisitReportCreateDoc struct {
+	ID          string     `json:"id" binding:"-"`
+	Type        string     `json:"type" binding:"-"`
+	Subject     string     `json:"subject" binding:"required" validate:"max=255"`
+	Description string     `json:"description" validate:"max=500"`
+	VisitDate   string     `json:"visitDate" binding:"required"`
+	Contact     ContactDoc `json:"contact"  binding:"required"`
+}
+
+// VisitReportUpdateDoc - struct for updating a VR
+type VisitReportUpdateDoc struct {
+	Type        string     `json:"type" binding:"-"`
+	Subject     string     `json:"subject" binding:"required" validate:"max=255"`
+	Description string     `json:"description" validate:"max=500"`
+	Result      string     `json:"result" validate:"max=500"`
+	VisitDate   string     `json:"visitDate" binding:"required"`
+	Contact     ContactDoc `json:"contact"  binding:"required"`
+}
+
+// VisitReportListDoc - struct for list operation
 type VisitReportListDoc struct {
-	cosmosapi.Document
-	Id      string     `json:"id"`
-	Type    string     `json:"type"`
-	Subject string     `json:"subject"`
-	Contact ContactDoc `json:"contact"`
+	ID        string     `json:"id"`
+	Type      string     `json:"type"`
+	Subject   string     `json:"subject"`
+	VisitDate string     `json:"visitDate"`
+	Contact   ContactDoc `json:"contact"`
 }
 
 var currentDb *cosmosapi.Database
@@ -48,7 +82,7 @@ func fromEnv() config {
 	cfg := config{}
 	if err := envconfig.Process("vr", &cfg); err != nil {
 		err = errors.WithStack(err)
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 
 	return cfg
@@ -56,14 +90,19 @@ func fromEnv() config {
 
 func main() {
 	r := gin.Default()
-
+	if os.Getenv("VR_ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
 	cfg := fromEnv()
 	currentCfg = &cfg
 	cosmosCfg := cosmosapi.Config{
 		MasterKey: currentCfg.DbKey,
 	}
 
-	currentClient = cosmosapi.New(currentCfg.DbUrl, cosmosCfg, nil, nil)
+	currentClient = cosmosapi.New(currentCfg.DbURL, cosmosCfg, nil, nil)
 
 	// Get a database
 	db, err := currentClient.GetDatabase(context.Background(), currentCfg.DbName, nil)
@@ -83,6 +122,7 @@ func main() {
 	})
 	r.GET("/reports", list)
 	r.GET("/reports/:reportid", read)
+	r.POST("/reports", update)
 	endless.ListenAndServe(":3000", r)
 }
 
@@ -122,11 +162,48 @@ func read(c *gin.Context) {
 		PartitionKeyValue: "visitreport",
 	}
 
-	var doc VisitReportDoc
+	var doc VisitReportReadDoc
 	_, err := currentClient.GetDocument(context.Background(), currentCfg.DbName, "visitreports", reportid, ro, &doc)
 	if err != nil {
 		err = errors.WithStack(err)
 		fmt.Println(err)
 	}
 	c.JSON(200, doc)
+}
+
+func update(c *gin.Context) {
+
+	// Create visit report
+	var vr VisitReportCreateDoc
+	if err := c.ShouldBindJSON(&vr); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	vr.Type = "visitreport"
+	vr.ID = uuid.New().String()
+	ops := cosmosapi.CreateDocumentOptions{}
+	ops = cosmosapi.CreateDocumentOptions{
+		PartitionKeyValue: "visitreport",
+	}
+	resource, _, err := currentClient.CreateDocument(context.Background(), currentCfg.DbName, "visitreports", vr, ops)
+	if err != nil {
+		err = errors.WithStack(err)
+		fmt.Println(err)
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	reportid := resource.Id
+	ro := cosmosapi.GetDocumentOptions{
+		PartitionKeyValue: "visitreport",
+	}
+
+	var doc VisitReportReadDoc
+	_, errRead := currentClient.GetDocument(context.Background(), currentCfg.DbName, "visitreports", reportid, ro, &doc)
+	if errRead != nil {
+		errRead = errors.WithStack(errRead)
+		fmt.Println(errRead)
+	}
+	c.JSON(201, doc)
 }
