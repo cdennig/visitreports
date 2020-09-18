2960e8a7bb7019370e96b06c71a16882a6ee3ea7
Christian Dennig / MS <dechrist@microsoft.com>
2020-09-18 10:01:10 +0200

    Introduce DAO struct

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 
 	servicebus "github.com/Azure/azure-service-bus-go"
@@ -21,13 +22,6 @@ import (
 	"github.com/vippsas/go-cosmosdb/cosmosapi"
 )
 
-type ListenerHandler struct {
-}
-
-func (l *ListenerHandler) Handle(c context.Context, m *servicebus.Message) error {
-	return nil
-}
-
 type config struct {
 	DbURL                string `required:"true"`
 	DbKey                string `required:"true"`
@@ -39,23 +33,18 @@ type config struct {
 
 // ContactDoc - Base contact properties
 type ContactDoc struct {
-	ID             string `json:"id" binding:"required"`
+	Id             string `json:"id" binding:"required"`
 	Firstname      string `json:"firstname"`
 	Lastname       string `json:"lastname"`
 	AvatarLocation string `json:"avatarLocation"`
 	Company        string `json:"company"`
 }
 
-// VisitReportEventDoc - struct for sending an event
-type VisitReportEventDoc struct {
-	EventType string `json:"eventType"`
-	Version   string `json:"version"`
-	VisitReportReadDoc
-}
-
-// VisitReportReadDoc - struct for reading a
-type VisitReportReadDoc struct {
-	ID                        string     `json:"id"`
+// VisitReportModel - struct for data access
+type VisitReportModel struct {
+	cosmosapi.Document
+	Type                      string     `json:"type"`
+	DetectedLanguage          string     `json:"detectedLanguage"`
 	Subject                   string     `json:"subject"`
 	Description               string     `json:"description"`
 	VisitDate                 string     `json:"visitDate"`
@@ -65,10 +54,27 @@ type VisitReportReadDoc struct {
 	Contact                   ContactDoc `json:"contact"`
 }
 
+// VisitReportReadDoc - struct for reading a
+type VisitReportReadDoc struct {
+	Id                        string     `json:"id"`
+	Subject                   string     `json:"subject"`
+	Description               string     `json:"description"`
+	VisitDate                 string     `json:"visitDate"`
+	Result                    string     `json:"result"`
+	VisitResultSentimentScore float64    `json:"visitResultSentimentScore"`
+	VisitResultKeyPhrases     []string   `json:"visitResultKeyPhrases"`
+	Contact                   ContactDoc `json:"contact"`
+}
+
+// VisitReportEventDoc - struct for sending an event
+type VisitReportEventDoc struct {
+	EventType string `json:"eventType"`
+	Version   string `json:"version"`
+	VisitReportReadDoc
+}
+
 // VisitReportCreateDoc - struct for creating a VR
 type VisitReportCreateDoc struct {
-	ID          string     `json:"id" binding:"-"`
-	Type        string     `json:"type" binding:"-"`
 	Subject     string     `json:"subject" binding:"required" validate:"max=255"`
 	Description string     `json:"description" validate:"max=500"`
 	VisitDate   string     `json:"visitDate" binding:"required"`
@@ -77,8 +83,7 @@ type VisitReportCreateDoc struct {
 
 // VisitReportUpdateDoc - struct for updating a VR
 type VisitReportUpdateDoc struct {
-	ID          string     `json:"id" binding:"required" validate:"uuid"`
-	Type        string     `json:"type" binding:"-"`
+	Id          string     `json:"id" binding:"required" validate:"uuid"`
 	Subject     string     `json:"subject" binding:"required" validate:"max=255"`
 	Description string     `json:"description" validate:"max=500"`
 	Result      string     `json:"result" validate:"max=500"`
@@ -88,11 +93,34 @@ type VisitReportUpdateDoc struct {
 
 // VisitReportListDoc - struct for list operation
 type VisitReportListDoc struct {
-	ID        string     `json:"id"`
+	Id        string     `json:"id"`
 	Type      string     `json:"type"`
 	Subject   string     `json:"subject"`
 	VisitDate string     `json:"visitDate"`
 	Contact   ContactDoc `json:"contact"`
+}
+
+// StatsByContactDoc - struct for list operation
+type StatsByContactDoc struct {
+	Id         string  `json:"id"`
+	CountScore float64 `json:"countScore"`
+	MinScore   float64 `json:"minScore"`
+	MaxScore   float64 `json:"maxScore"`
+	AvgScore   float64 `json:"avgScore"`
+}
+
+// StatsOverallDoc - struct for list operation
+type StatsOverallDoc struct {
+	CountScore float64 `json:"countScore"`
+	MinScore   float64 `json:"minScore"`
+	MaxScore   float64 `json:"maxScore"`
+	AvgScore   float64 `json:"avgScore"`
+}
+
+// StatsTimelineDoc - struct for list operation
+type StatsTimelineDoc struct {
+	VisitDate string `json:"visitDate"`
+	Visits    int16  `json:"visits"`
 }
 
 var currentDb *cosmosapi.Database
@@ -140,13 +168,70 @@ func setupSubscription() error {
 
 	receiver, err := sub.NewReceiver(context.Background())
 	lHandle := receiver.Listen(context.Background(), servicebus.HandlerFunc(func(c context.Context, m *servicebus.Message) error {
-		return nil
+		doc := ContactDoc{}
+		err := json.Unmarshal(m.Data, &doc)
+		if err != nil {
+			m.Abandon(context.Background())
+			fmt.Println(err)
+		}
+
+		if err != nil {
+			fmt.Println(err)
+		}
+		qops := cosmosapi.DefaultQueryDocumentOptions()
+		qops.PartitionKeyValue = "visitreport"
+		var qry cosmosapi.Query
+
+		qry = cosmosapi.Query{
+			Query: "SELECT * FROM c where c.contact.id = @contactid",
+			Params: []cosmosapi.QueryParam{
+				{
+					Name:  "@contactid",
+					Value: doc.Id,
+				},
+			},
+		}
+
+		var docs []VisitReportModel
+		_, errQuery := currentClient.QueryDocuments(context.Background(), currentCfg.DbName, "visitreports", qry, &docs, qops)
+		if errQuery != nil {
+			errQuery = errors.WithStack(errQuery)
+			fmt.Println(errQuery)
+		}
+
+		var wg sync.WaitGroup
+		for _, v := range docs {
+			wg.Add(1)
+			go updateInBg(v, &doc, &wg)
+		}
+		wg.Wait()
+
+		err = m.Complete(context.Background())
+		return err
 	}))
 
 	if lHandle == nil {
 		fmt.Println("Not init.")
 	}
 	return nil
+}
+
+func updateInBg(doc VisitReportModel, contact *ContactDoc, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("Processing.... Id %s \n", doc.Id)
+	ops := cosmosapi.ReplaceDocumentOptions{}
+	ops.PartitionKeyValue = "visitreport"
+	doc.Contact.Firstname = contact.Firstname
+	doc.Contact.Lastname = contact.Lastname
+	doc.Contact.AvatarLocation = contact.AvatarLocation
+	doc.Contact.Company = contact.Company
+	doc.Type = "visitreport"
+	_, res, err := currentClient.ReplaceDocument(context.Background(), currentCfg.DbName, "visitreports", doc.Id, doc, ops)
+	if err != nil {
+		err = errors.WithStack(err)
+		fmt.Println(err)
+	}
+	fmt.Printf("Request Units: %f\n", res.RUs)
 }
 
 func main() {
@@ -175,12 +260,13 @@ func main() {
 	currentDb = db
 
 	currentTopic, err = setupTopicSender()
-	setupSubscription()
-
 	if err != nil {
 		err = errors.WithStack(err)
 		fmt.Println(err)
 	}
+
+	setupSubscription()
+
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "DELETE", "PUT", "POST", "OPTIONS"},
@@ -205,6 +291,9 @@ func main() {
 	r.PUT("/reports/:reportid", update)
 	r.DELETE("/reports/:reportid", delete)
 	r.POST("/reports", create)
+	r.GET("/stats", readStatsOverall)
+	r.GET("/stats/:contactid", readStatsByContactID)
+	r.GET("/timeline", readStatsTimeline)
 	endless.ListenAndServe(":3000", r)
 }
 
@@ -229,13 +318,15 @@ func list(c *gin.Context) {
 		}
 	}
 
-	var docs []VisitReportListDoc
+	var docs []VisitReportModel
 	_, err := currentClient.QueryDocuments(context.Background(), currentCfg.DbName, "visitreports", qry, &docs, qops)
 	if err != nil {
 		err = errors.WithStack(err)
 		fmt.Println(err)
 	}
-	c.JSON(http.StatusOK, docs)
+	out := []VisitReportListDoc{}
+	copier.Copy(&out, &docs)
+	c.JSON(http.StatusOK, out)
 }
 
 func read(c *gin.Context) {
@@ -244,13 +335,15 @@ func read(c *gin.Context) {
 		PartitionKeyValue: "visitreport",
 	}
 
-	var doc VisitReportReadDoc
+	var doc VisitReportModel
 	_, err := currentClient.GetDocument(context.Background(), currentCfg.DbName, "visitreports", reportid, ro, &doc)
 	if err != nil {
 		err = errors.WithStack(err)
 		fmt.Println(err)
 	}
-	c.JSON(http.StatusOK, doc)
+	out := VisitReportReadDoc{}
+	copier.Copy(&out, &doc)
+	c.JSON(http.StatusOK, out)
 }
 
 func delete(c *gin.Context) {
@@ -268,16 +361,15 @@ func delete(c *gin.Context) {
 }
 
 func create(c *gin.Context) {
+	vr := VisitReportModel{}
 
-	// Create visit report
-	var vr VisitReportCreateDoc
 	if err := c.ShouldBindJSON(&vr); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	vr.Type = "visitreport"
-	vr.ID = uuid.New().String()
+	vr.Id = uuid.New().String()
 	ops := cosmosapi.CreateDocumentOptions{}
 	ops = cosmosapi.CreateDocumentOptions{
 		PartitionKeyValue: "visitreport",
@@ -295,7 +387,7 @@ func create(c *gin.Context) {
 		PartitionKeyValue: "visitreport",
 	}
 
-	var doc VisitReportReadDoc
+	var doc VisitReportModel
 	_, errRead := currentClient.GetDocument(context.Background(), currentCfg.DbName, "visitreports", reportid, ro, &doc)
 	if errRead != nil {
 		errRead = errors.WithStack(errRead)
@@ -324,8 +416,9 @@ func create(c *gin.Context) {
 		fmt.Printf("Error: %s", err)
 		return
 	}
-
-	c.JSON(http.StatusCreated, doc)
+	out := VisitReportReadDoc{}
+	copier.Copy(&out, &doc)
+	c.JSON(http.StatusCreated, out)
 }
 
 func update(c *gin.Context) {
@@ -336,12 +429,22 @@ func update(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": bindError.Error()})
 		return
 	}
+	ro := cosmosapi.GetDocumentOptions{
+		PartitionKeyValue: "visitreport",
+	}
+	model := VisitReportModel{}
 
-	vr.Type = "visitreport"
+	_, err := currentClient.GetDocument(context.Background(), currentCfg.DbName, "visitreports", reportid, ro, &model)
+	if err != nil {
+		err = errors.WithStack(err)
+		fmt.Println(err)
+	}
+
+	copier.Copy(&model, &vr)
 	ops := cosmosapi.ReplaceDocumentOptions{}
 	ops.PartitionKeyValue = "visitreport"
 
-	_, _, err := currentClient.ReplaceDocument(context.Background(), currentCfg.DbName, "visitreports", reportid, vr, ops)
+	_, _, err = currentClient.ReplaceDocument(context.Background(), currentCfg.DbName, "visitreports", reportid, model, ops)
 
 	if err != nil {
 		err = errors.WithStack(err)
@@ -350,22 +453,11 @@ func update(c *gin.Context) {
 		return
 	}
 
-	ro := cosmosapi.GetDocumentOptions{
-		PartitionKeyValue: "visitreport",
-	}
-
-	var doc VisitReportReadDoc
-	_, errRead := currentClient.GetDocument(context.Background(), currentCfg.DbName, "visitreports", reportid, ro, &doc)
-	if errRead != nil {
-		errRead = errors.WithStack(errRead)
-		fmt.Println(errRead)
-	}
-
 	// send event
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	eventDoc := VisitReportEventDoc{}
-	copier.Copy(&eventDoc, &doc)
+	copier.Copy(&eventDoc, &model)
 	eventDoc.EventType = "VisitReportUpdatedEvent"
 	eventDoc.Version = "1"
 	m, err := json.Marshal(eventDoc)
@@ -383,5 +475,71 @@ func update(c *gin.Context) {
 		fmt.Printf("Error: %s", err)
 		return
 	}
+	doc := VisitReportReadDoc{}
+	copier.Copy(&model, &doc)
 	c.JSON(http.StatusOK, doc)
+}
+
+func readStatsByContactID(c *gin.Context) {
+	contactid := c.Param("contactid")
+	qops := cosmosapi.DefaultQueryDocumentOptions()
+	qops.PartitionKeyValue = "visitreport"
+	qry := cosmosapi.Query{
+		Query: "SELECT c.contact.id, COUNT(1) as countScore, AVG(c.visitResultSentimentScore) as avgScore, MAX(c.visitResultSentimentScore) as maxScore, MIN(c.visitResultSentimentScore) as minScore FROM c WHERE c.type = 'visitreport' and c.result != ''  AND c.contact.id = @contactid GROUP BY c.contact.id",
+		Params: []cosmosapi.QueryParam{
+			{
+				Name:  "@contactid",
+				Value: contactid,
+			},
+		},
+	}
+	var docs []StatsByContactDoc
+	_, err := currentClient.QueryDocuments(context.Background(), currentCfg.DbName, "visitreports", qry, &docs, qops)
+	if err != nil {
+		err = errors.WithStack(err)
+		fmt.Println(err)
+	}
+	c.JSON(http.StatusOK, docs)
+}
+
+func readStatsOverall(c *gin.Context) {
+	qops := cosmosapi.DefaultQueryDocumentOptions()
+	qops.PartitionKeyValue = "visitreport"
+	qry := cosmosapi.Query{
+		Query: `SELECT
+					COUNT(1) as countScore,
+					AVG(c.visitResultSentimentScore) as avgScore,
+					MAX(c.visitResultSentimentScore) as maxScore,
+					MIN(c.visitResultSentimentScore) as minScore 
+				FROM c
+				WHERE c.type = 'visitreport' and c.result != ''
+				GROUP BY c.type`,
+	}
+	var docs []StatsOverallDoc
+	_, err := currentClient.QueryDocuments(context.Background(), currentCfg.DbName, "visitreports", qry, &docs, qops)
+	if err != nil {
+		err = errors.WithStack(err)
+		fmt.Println(err)
+	}
+	c.JSON(http.StatusOK, docs)
+}
+
+func readStatsTimeline(c *gin.Context) {
+	qops := cosmosapi.DefaultQueryDocumentOptions()
+	qops.PartitionKeyValue = "visitreport"
+	qry := cosmosapi.Query{
+		Query: `SELECT
+				c.visitDate,
+				COUNT(1) as visits
+				FROM c
+				WHERE c.type = 'visitreport' AND c.result != ''
+				GROUP BY c.visitDate`,
+	}
+	var docs []StatsTimelineDoc
+	_, err := currentClient.QueryDocuments(context.Background(), currentCfg.DbName, "visitreports", qry, &docs, qops)
+	if err != nil {
+		err = errors.WithStack(err)
+		fmt.Println(err)
+	}
+	c.JSON(http.StatusOK, docs)
 }
